0ee69fe3d02af2b08bb073f9f54891dd7faa2e06
bloomhealthy <144273062+bloomhealthy@users.noreply.github.com>
2023-09-14 10:08:35 +0300

    Add files via upload
    Signed-off-by: bloomhealthy <144273062+bloomhealthy@users.noreply.github.com>

--- Bloom/App/Main.js
+++ Bloom/App/Main.js
@@ -2,6 +2,21 @@ const DIV=document.querySelector('div');
 
 window.onload = PAGECONTROL;
 
+ZOOM = () => {
+    // Disable zooming by preventing the default behavior of touchstart and wheel events
+    document.addEventListener('touchstart', function(event) {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    }, { passive: false });
+  
+    document.addEventListener('wheel', function(event) {
+      if ((event.ctrlKey || event.metaKey) && !event.shiftKey) {
+        event.preventDefault();
+      }
+    });
+};
+
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
