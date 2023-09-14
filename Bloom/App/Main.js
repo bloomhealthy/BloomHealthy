139c7d75add8d37eeb70d2672c09302558a7759b
bloomhealthy <144273062+bloomhealthy@users.noreply.github.com>
2023-09-14 09:47:46 +0300

    Add files via upload
    Signed-off-by: bloomhealthy <144273062+bloomhealthy@users.noreply.github.com>

--- Bloom/App/Main.js
+++ Bloom/App/Main.js
@@ -1,3 +1,13 @@
 const DIV=document.querySelector('div');
 
 window.onload = PAGECONTROL;
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register('/ASSETS/service-worker.js').then(function(registration) {
+            console.log('ServiceWorker registered');
+          }).catch(function(err) {
+            console.log('ServiceWorker error: ', err);
+          });
+    })
+}
